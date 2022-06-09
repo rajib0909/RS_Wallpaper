d77dd0056d902a3ee0eba5c60ccc73a1bb2817ce
tanvir0909 <rajib0909@gmail.com>
2022-06-10 02:10:06 +0600

    Social login running

--- app/src/main/java/com/rsdesign/wallpaper/view/LoginActivity.java
+++ app/src/main/java/com/rsdesign/wallpaper/view/LoginActivity.java
@@ -2,15 +2,97 @@ package com.rsdesign.wallpaper.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.google.android.material.button.MaterialButton;
 import com.rsdesign.wallpaper.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginActivity extends AppCompatActivity {
+
+    //GoogleSignInClient googleSignInClient;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        MaterialButton fbButton = findViewById(R.id.btn_facebook);
+
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+
+
+                        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+                        GraphRequest request = GraphRequest.newMeRequest(
+                                accessToken,
+                                new GraphRequest.GraphJSONObjectCallback() {
+                                    @Override
+                                    public void onCompleted(
+                                            JSONObject object,
+                                            GraphResponse response) {
+                                        // Application code
+                                        try {
+                                            Map<String, String> accountMap = new HashMap<>();
+                                            accountMap.put("uid", object.getString("id"));
+                                            accountMap.put("name", object.getString("name"));
+                                            accountMap.put("provider", "facebook");
+                                           // socialLogin(accountMap);
+                                            Log.d("fbid", object.getString("id"));
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                });
+                        Bundle parameters = new Bundle();
+                        parameters.putString("fields", "id,name,link");
+                        request.setParameters(parameters);
+                        request.executeAsync();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+
+
+        fbButton.setOnClickListener(l -> {
+
+            LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+        });
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
